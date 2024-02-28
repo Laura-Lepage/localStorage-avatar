d9929ed85904f7eb2174c669e5ceb1f26e3ebac9
Laura-Lepage <lauralepage02@hotmail.com>
2024-02-28 11:55:15 +0100

    correction terminée

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,9 @@ let inputUrl = document.querySelector("#url")
 let backButton = document.querySelector(".back")
 
 //Remplir le formulaire avec les données du localStorage s'il y en a
-if (localStorage.getItem('inputName') && localStorage.getItem('inputUrl')) {
-    nameInput.value = localStorage.getItem('inputNameValue');
-    avatarInput.value = localStorage.getItem('inputUrlValue');
+if (localStorage.getItem('name') && localStorage.getItem('avatarLink')) {
+    inputName.value = localStorage.getItem('name'); //'name' = la clé qui contiendra la valeur de l'inputName
+    inputUrl.value = localStorage.getItem('avatarLink'); //'avatarLink' = la clé qui contiendra la valeur de l'inputUrl
 }
 
 function displayUser() {
@@ -16,7 +16,6 @@ function displayUser() {
     if (inputName.value !== "" && inputUrl.value !== "") {
         form.classList.add("slide_form")
         avatar.classList.add("slide_avatar")
-        
     // Injection du contenu en HTML 
         avatar.innerHTML = 
         `
@@ -25,28 +24,41 @@ function displayUser() {
         <div class="userbonjour">Bonjour, ${inputName.value} !</div>
         
         `  
+    // Enregistrement des données dans le localStorage
+        localStorage.setItem('name', inputName.value);
+        localStorage.setItem('avatarLink', inputUrl.value);
 
-//         // Injection du contenu en HTML
-//         circle.style.background = `url(${avatarInput.value}) center / cover`;
-//         userInfo.innerHTML = `
-//             <p>Bonjour, ${nameInput.value}.</p>
-//         `;
-//         // Enregistrement des données dans le local storage
-//         localStorage.setItem("name", nameInput.value);
-//         localStorage.setItem("avatar", avatarInput.value);
+    // Réinitialisation des valeurs du formulaire
+        inputName.value = "";
+        inputUrl.value = "";
 
-//         // Réinitialisation des valeurs du formulaire
-//         nameInput.value = "";
-//         avatarInput.value = "";
-//     } else {
-//         alert("Veuillez remplir tous les champs.");
-//     }
-// }
-
+    } else {
+        alert("Veuillez remplir tous les champs.");
+    }
+}
 
 button.addEventListener('click', function(){
+    displayUser()
+    inputName.value = localStorage.getItem("inputName");
+    inputUrl.value = localStorage.getItem("inputUrl");
     
 })
+
+// au chargement de la page, on vérifie si les données avaient déjà été remplies et, si oui, on affiche directement la box avatar
+if(localStorage.getItem('name') != "" && localStorage.getItem('avatarLink')){
+    form.classList.add("slide_form")
+    avatar.classList.add("slide_avatar")
+    avatar.innerHTML = 
+    `
+    <button class="back">🔙</button>
+    <div class="image" style="background: url('${inputUrl.value}') center/cover"></div>
+    <div class="userbonjour">Bonjour, ${inputName.value} !</div>
+    
+    ` 
+    inputName.value = localStorage.getItem('name');
+    inputUrl.value = localStorage.getItem('avatarLink');
+
+}
 
 avatar.addEventListener('click', function(e){
     if (e.target.classList.contains('back')) {
@@ -55,57 +67,4 @@ avatar.addEventListener('click', function(e){
     }
 })
 
-
 //https://cache.cosmopolitan.fr/data/photo/w1200_h630_c17/4u/rebelle.jpg
-
-// // Déclaration de variables
-// let form = document.querySelector(".form"),
-//     nameInput = document.querySelector(".name"),
-//     avatarInput = document.querySelector(".avatar"),
-//     button = document.querySelector("button"),
-//     show = document.querySelector(".showBox"),
-//     circle = document.querySelector(".circle"),
-//     back = document.querySelector(".back"),
-//     userInfo = document.querySelector(".user-info");
-
-// // Remplir le formulaire avec les données du local storage s'il y en a
-// if (localStorage.getItem("name") && localStorage.getItem("avatar")) {
-//     nameInput.value = localStorage.getItem("name");
-//     avatarInput.value = localStorage.getItem("avatar");
-// }
-
-// function displayUser() {
-//     // Vérifier si les champs ne sont pas vides
-//     if (nameInput.value !== "" && avatarInput.value !== "") {
-//         form.classList.add("slide_form");
-//         show.classList.add("slide_box");
-
-//         // Injection du contenu en HTML
-//         circle.style.background = `url(${avatarInput.value}) center / cover`;
-//         userInfo.innerHTML = `
-//             <p>Bonjour, ${nameInput.value}.</p>
-//         `;
-//         // Enregistrement des données dans le local storage
-//         localStorage.setItem("name", nameInput.value);
-//         localStorage.setItem("avatar", avatarInput.value);
-
-//         // Réinitialisation des valeurs du formulaire
-//         nameInput.value = "";
-//         avatarInput.value = "";
-//     } else {
-//         alert("Veuillez remplir tous les champs.");
-//     }
-// }
-
-// // Évenement sur le bouton "Envoyer"
-// button.addEventListener("click", function(){
-//     displayUser()
-//     nameInput.value = localStorage.getItem("name");
-//     avatarInput.value = localStorage.getItem("avatar");
-// });
-
-// // Évenement sur le bouton "Retour"
-// back.addEventListener("click", function(){
-//     form.classList.remove("slide_form");
-//     show.classList.remove("slide_box");
-// });
